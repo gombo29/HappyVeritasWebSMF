c6b39d7569eceea80a3606fe0f0a195b9d3c0fbf
gombo29 <gombo29@gmail.com>
2017-02-21 12:02:23 +0800

    q

---
+++ src/happy/WebBundle/Controller/DefaultController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace happy\WebBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+
+class DefaultController extends Controller
+{
+    /**
+     * @Route("/")
+     */
+    public function indexAction()
+    {
+        return $this->render('happyWebBundle:Default:index.html.twig');
+    }
+}
